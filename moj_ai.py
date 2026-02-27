343c4aa0719b5476ce695028a28e4e3d7805af66
Mirjana Krunic <mirjanakrunic11@gmail.com>
2026-02-27 17:01:50 +0100

    Mentor je online i koristi Gemini 2.5

--- moj_ai.py
+++ moj_ai.py
@@ -2,11 +2,9 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-# 1. Učitavanje
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
-# 2. Podešavanje klijenta
 client = genai.Client(api_key=api_key)
 
 print("\n--- TVOJ AI MENTOR JE ONLINE ---")
@@ -18,10 +16,8 @@ while True:
         break
 
     try:
-        # Obriši onaj 'for' krug što smo dodali za listanje, više nam ne treba
-        
         response = client.models.generate_content(
-            model="gemini-2.5-flash", # Koristimo tačno ime sa tvog ekrana (bez models/ prefiksa često radi bolje)
+            model="gemini-2.5-flash", 
             config={
                 'system_instruction': "Ti si moj ljubazni Python mentor. Ohrabruj me i povremeno mi postavi po jedan mali zadatak.",
             },
